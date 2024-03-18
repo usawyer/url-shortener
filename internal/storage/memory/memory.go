023ff9ce012b567fbc370636a5b831a1afb2d217
yarina <yarinabelousova@gmail.com>
2024-03-18 20:44:43 +0700

    deleting openApi.yml

--- internal/storage/memory/memory.go
+++ internal/storage/memory/memory.go
@@ -23,7 +23,7 @@ func (c *RedisClient) GetUrl(ctx context.Context, key string) (string, error) {
 	value, err := c.rd.Get(ctx, key).Result()
 	if err != nil {
 		if err == redis.Nil {
-			return "", fmt.Errorf("URL with alias \"%s\" doesn't found", key)
+			return "", fmt.Errorf("URL with alias \"%s\" not found", key)
 		}
 		return "", err
 	}
